33d9e57823fa9704fe0c456b993e90f0791e0eb0
SezamParmezan <galiyevru07@gmail.com>
2026-04-19 13:49:56 +0200

    linux fix

--- start.sh
+++ start.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")/app"
-
 if [ ! -d ".venv" ]; then
-    echo "Setting up for the first time..."
+    echo "Setting up..."
     python3 -m venv .venv
-    source .venv/bin/activate
-    pip install -r requirements.txt
-else
-    source .venv/bin/activate
+    .venv/bin/pip install -r requirements.txt
 fi
-
 echo "Starting AI Chess..."
 open http://127.0.0.1:8000 2>/dev/null || xdg-open http://127.0.0.1:8000
-uvicorn api.main:chess --host 127.0.0.1 --port 8000
+.venv/bin/uvicorn api.main:chess --host 127.0.0.1 --port 8000
